8989e2880ed34d78d256954a578b39efd1252582
arshad012 <forarshad99@gmail.com>
2024-12-17 13:00:37 +0530

    First question

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
-import React from "react";
+import {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text, setText] = useState('');
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <input onChange={(e) => setText(e.target.value)} />
+        <h1>{text}</h1>
     </div>
   )
 }
